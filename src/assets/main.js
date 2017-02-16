7034d1db5a5c437198cf73aca266fcc2e48dd5f7
Daniel Max Nielsen <dmn@translucent.dk>
2017-02-16 10:24:05 +0100

    more stuff

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,19 +4,19 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer === '' || attempt === '') {
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
 function setHiddenFields() {
-    answer = Math.random() * (9999 - 0) + 0;
-    answer = Math.floor(answer);
-    answer = answer.toString();
-    while (answer.length < 4) {
-        answer = 0 + answer;
+    answer.value = Math.random() * (9999 - 0) + 0;
+    answer.value = Math.floor(answer.value);
+    answer.value = answer.value.toString();
+    while (answer.value.length < 4) {
+        answer.value = 0 + answer.value;
     }
-    console.log(answer);
     
-    attempt = 0;
+    attempt.value = 0;
 }
-
-setHiddenFields();
