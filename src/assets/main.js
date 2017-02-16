9303219a07b58096a4fa98e548d0aae43a5170d6
Daniel Max Nielsen <dmn@translucent.dk>
2017-02-16 11:39:41 +0100

    adds getResult function

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,12 +1,19 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 let messageLabel = document.getElementById('message');
+let results = document.getElementById('results');
 
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
     if (answer === '' || attempt === '') {
         setHiddenFields();
+    }
+    
+    if (!validateInput(input.value)) {
+        return false;
+    } else {
+        attempt.value = attempt.value + 1;
     }
 }
 
@@ -33,4 +40,21 @@ function validateInput(input) {
         setMessage("Guesses must be exactly 4 characters long.");
         return false;
     }
+}
+
+function getResults(guess) {
+    guessArray = guess.toString().split("");
+    answerArray = answer.value.toString().split("");
+    let result = '<div class="row"><span class="col-md-6">' + guess + '</span><div class="col-md-6">';
+    for (let i = 0; i < 4; i++) {
+        if (guessArray[i] === answerArray[i]) {
+            result = result + '<span class="glyphicon glyphicon-ok"></span>';
+        } else if(answerArray.includes(guessArray[i])) {
+            result = result + '<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            result = result + '<span class="glyphicon glyphicon-remove"></span>';      
+        }
+    }
+    result = result + '</div></div>';
+    results.innerHTML = results.innerHTML + result;
 }
